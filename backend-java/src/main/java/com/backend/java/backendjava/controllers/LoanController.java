ce3e03eb5371082eb6709902fa2893819cc17da9
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-03-27 16:27:33 -0300

    commited by Abner

--- backend-java/src/main/java/com/backend/java/backendjava/controllers/LoanController.java
+++ backend-java/src/main/java/com/backend/java/backendjava/controllers/LoanController.java
@@ -3,6 +3,7 @@ package com.backend.java.backendjava.controllers;
 import com.backend.java.backendjava.dtos.requests.LoanRequest;
 import com.backend.java.backendjava.dtos.responses.LoanResponse;
 import com.backend.java.backendjava.services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/loans")
 public class LoanController {
+    @Autowired
     private LoanService loanService;
-
-    public LoanController(LoanService loanService) {
-        this.loanService = loanService;
-    }
 
     @PostMapping("/customer-loans")
     public ResponseEntity<LoanResponse> grantLoan(LoanRequest request) {
